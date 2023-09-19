b5519853920945b70c134edd91278c596ee18986
Cole <coleb229@yahoo.com>
2023-09-18 22:41:32 -0700

    fixing deployment

--- lib/auth.ts
+++ lib/auth.ts
@@ -7,8 +7,8 @@ export const authOptions: NextAuthOptions = {
   },
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
 }
