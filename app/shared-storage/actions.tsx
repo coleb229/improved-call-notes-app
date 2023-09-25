9b9876c0096bf50509aac6cc5a79a353421aba00
Cole <coleb229@yahoo.com>
2023-09-24 20:11:47 -0700

    Hoping to fix date with this one

--- app/shared-storage/actions.tsx
+++ app/shared-storage/actions.tsx
@@ -9,6 +9,12 @@ const getDate = (givenDate = new Date()): string => {
   return givenDate.toISOString().split('T')[0];
 };
 
+const getDateEnd = (givenDate = new Date()): string => {
+  const offset = givenDate.getTimezoneOffset();
+  givenDate = new Date(new Date(givenDate.getTime() - offset * 60 * 1000).setHours(23,59,59,999));
+  return givenDate.toISOString().split('T')[0];
+};
+
 export const fetchCallNotes = async () => {
   try {
     const callNote = await prisma.callNote.findMany({
@@ -51,7 +57,7 @@ export const fetchDaysHandoffs = async () => {
       where: {
         createdAt: {
           gte: new Date(getDate()),
-          lt: new Date(new Date().setHours(23,59,59,999)),
+          lt: new Date(getDateEnd()),
         }
       },
     })
