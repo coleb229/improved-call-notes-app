aab54d492d65d99f70062e52885c30559142c93b
Cole <coleb229@yahoo.com>
2023-10-04 22:54:33 -0700

    Fixed a bug with tailwind messing with the navbar

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,14 +1,16 @@
 "use server"
 import ExternalHandoff from "./components/ExternalHandoffForm"
 import ExternalHandoffOutput from "./components/ExternalHandoffOutput"
+import TestingStages from "./components/TestingStages"
 
 export default async function Home() {
   return (
-    <main className="max-h-screen mt-[100px]">
-      <div className="flex">
+    <main className="mt-[100px]">
+      <div className="flex" id="feature-testing">
         <ExternalHandoff />
         <ExternalHandoffOutput />
       </div>
+      <TestingStages />
     </main>
   )
 }
